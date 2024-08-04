98e0c4c984c8edf8fda6e1d299f24eb4c6823ef0
Antony Agrios <antonyagrios@gmail.com>
2024-08-04 23:02:03 +0300

    Update: Fix lint issues

--- test/fixtures/mockRandomGenerator.sol
+++ test/fixtures/mockRandomGenerator.sol
@@ -4,9 +4,7 @@ pragma solidity ^0.8.4;
 import "../../src/IRandomNumberGenerator.sol";
 
 contract MockRandomNumberGenerator is IRandomNumberGenerator {
-    function getRandomNumber(
-        uint256 _seed
-    ) external pure override returns (uint256) {
+    function getRandomNumber(uint256 _seed) external pure override returns (uint256) {
         return _seed;
     }
 }
--- test/fixtures/mockVerifier.sol
+++ test/fixtures/mockVerifier.sol
@@ -4,12 +4,7 @@ pragma solidity ^0.8.4;
 import "../../src/IAttributeVerifier.sol";
 
 contract mockVerifier is IAttributeVerifier {
-    function verify(
-        bytes32,
-        bytes32[] calldata,
-        uint256,
-        uint256
-    ) public pure returns (bool) {
+    function verify(bytes32, bytes32[] calldata, uint256, uint256) public pure returns (bool) {
         return true;
     }
 }
